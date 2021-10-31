155b8503a7f446818523f805954ae7d3a67d8efa
BKlemm <bkl3mm@googlemail.com>
2021-10-31 14:45:39 +0100

    [AV]: #extend starting production stack

--- api/src/main/java/com/avondock/app/configuration/WebSecurityConfig.java
+++ api/src/main/java/com/avondock/app/configuration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         String[] permitted = new String[]{
-                "/**", "/static/**", "/register", "/login", "/api/**","/swagger-ui/**",
+                "/**", "/public/**", "/register", "/login", "/api/**","/swagger-ui/**",
                 "https://www.sandbox.paypal.com/**"
         };
         httpSecurity
@@ -64,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Profile("dev")
     CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(ImmutableList.of("*"));
