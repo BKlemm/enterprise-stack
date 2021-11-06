0cf97901e758f69921709a5075a16ab8595a1bc2
BKlemm <bkl3mm@googlemail.com>
2021-11-06 22:22:56 +0100

    [AV-FE]: #fix Localize injection

--- frontend/apps/administration/src/polyfills.ts
+++ frontend/apps/administration/src/polyfills.ts
@@ -62,3 +62,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import '@angular/localize/init';
