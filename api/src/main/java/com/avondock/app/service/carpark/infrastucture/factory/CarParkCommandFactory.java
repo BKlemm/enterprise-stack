ceacca16deedd5816ab0296f7bfc7b97327ac3ed
BKlemm <bkl3mm@googlemail.com>
2021-11-13 11:59:00 +0100

    [AV-API]: #implement #commandfactory use factory to create similar commands

---
+++ api/src/main/java/com/avondock/app/service/carpark/infrastucture/factory/CarParkCommandFactory.java
@@ -0,0 +1,47 @@
+package com.avondock.app.service.carpark.infrastucture.factory;
+
+import com.avondock.app.service.carpark.cqrs.coreapi.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+
+public class CarParkCommandFactory {
+
+    public static AbstractCarParkCommand create(AbstractCarParkDTO request, Class<?> command, CarParkId id) {
+        Constructor<?> ctor;
+        try {
+            ctor = command.getDeclaredConstructor(CarParkId.class, String.class, String.class, String.class,
+                    CarParkAddress.class, String.class, String.class, BigDecimal.class, CarParkStatus.class);
+        } catch (NoSuchMethodException noSuchMethodException) {
+            return null;
+        }
+
+        try {
+            return (AbstractCarParkCommand) ctor.newInstance(
+                    id,
+                    request.getIataCode(),
+                    request.getName(),
+                    request.getDescription(),
+                    new CarParkAddress(
+                            request.getAddress().getStreet(),
+                            request.getAddress().getNumber(),
+                            request.getAddress().getCity(),
+                            request.getAddress().getZip(),
+                            request.getAddress().getCountry(),
+                            request.getAddress().getRegion(),
+                            request.getAddress().getLatitude(),
+                            request.getAddress().getLongitude()
+                    ),
+                    request.getSupportEmail(),
+                    request.getSupportPhone(),
+                    request.getTax(),
+                    request.getState()
+            );
+        } catch(InvocationTargetException | IllegalAccessException | InstantiationException invocationTargetException) {
+            return null;
+        }
+
+
+    }
+}
