f200b99d628c4b498c2842f5c46174734bc92434
BKlemm <bkl3mm@googlemail.com>
2021-11-14 12:39:52 +0100

    [AV-API]: #change #carpark-factory simplify create method and use Modelmapper for equal pojos

--- api/src/main/java/com/avondock/app/service/carpark/infrastucture/factory/CarParkCommandFactoryImpl.java
+++ api/src/main/java/com/avondock/app/service/carpark/infrastucture/factory/CarParkCommandFactoryImpl.java
@@ -2,47 +2,32 @@ package com.avondock.app.service.carpark.infrastucture.factory;
 
 import com.avondock.app.service.carpark.cqrs.command.factory.CarParkCommandFactory;
 import com.avondock.app.service.carpark.cqrs.coreapi.*;
+import com.avondock.core.shared.infrastructure.factory.CommandFactoryImpl;
+import org.modelmapper.ModelMapper;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 
-public class CarParkCommandFactoryImpl implements CarParkCommandFactory {
+public class CarParkCommandFactoryImpl extends CommandFactoryImpl implements CarParkCommandFactory {
 
     public AbstractCarParkCommand create(AbstractCarParkDTO request, Class<?> command, CarParkId id) {
-        Constructor<?> ctor;
-        try {
-            ctor = command.getDeclaredConstructor(CarParkId.class, String.class, String.class, String.class,
-                    CarParkAddress.class, String.class, String.class, BigDecimal.class, CarParkStatus.class);
-        } catch (NoSuchMethodException noSuchMethodException) {
-            return null;
-        }
+        this.construct(command,
+            CarParkId.class, String.class, String.class, String.class,
+            CarParkAddress.class, String.class, String.class, BigDecimal.class, CarParkStatus.class
+        );
 
-        try {
-            return (AbstractCarParkCommand) ctor.newInstance(
-                    id,
-                    request.getIataCode(),
-                    request.getName(),
-                    request.getDescription(),
-                    new CarParkAddress(
-                            request.getAddress().getStreet(),
-                            request.getAddress().getNumber(),
-                            request.getAddress().getCity(),
-                            request.getAddress().getZip(),
-                            request.getAddress().getCountry(),
-                            request.getAddress().getRegion(),
-                            request.getAddress().getLatitude(),
-                            request.getAddress().getLongitude()
-                    ),
-                    request.getSupportEmail(),
-                    request.getSupportPhone(),
-                    request.getTax(),
-                    request.getState()
-            );
-        } catch(InvocationTargetException | IllegalAccessException | InstantiationException invocationTargetException) {
-            return null;
-        }
+        ModelMapper mapper = new ModelMapper();
+        CarParkAddress address = mapper.map(request.getAddress(), CarParkAddress.class);
 
-
+        return (AbstractCarParkCommand) this.createInstance(
+            id,
+            request.getIataCode(),
+            request.getName(),
+            request.getDescription(),
+            address,
+            request.getSupportEmail(),
+            request.getSupportPhone(),
+            request.getTax(),
+            request.getState()
+        );
     }
 }
