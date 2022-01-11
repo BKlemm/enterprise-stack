625c86e87917ab601436778b6b1146008cb40513
BKlemm <bkl3mm@googlemail.com>
2022-01-11 20:02:26 +0100

    [AV-FE]: #extend shared-core:decorators with gateway option

--- frontend/libs/shared/core/src/lib/decorators/core.ts
+++ frontend/libs/shared/core/src/lib/decorators/core.ts
@@ -7,94 +7,96 @@ export function V1<T extends Constructor>(constructor: T) {
 }
 
 export function Resource(resource: string) {
-    return function <T extends Constructor>(constructor: T){
-        return class extends constructor {
-            resource = resource
-        }
+  return function <T extends Constructor>(constructor: T){
+    return class extends constructor {
+      resource = resource
     }
+  }
 }
 
 export function Version(version: string) {
-    return function <T extends Constructor>(constructor: T){
-        return class extends constructor {
-            version = version
-        }
+  return function <T extends Constructor>(constructor: T){
+    return class extends constructor {
+      version = version
     }
+  }
 }
 
 interface Parameters {
-    version?: string,
-    resource?: string
+  gateway?: string
+  version?: string,
+  resource?: string
 }
 
 export function Route(params: Parameters|any ) {
-    return function <T extends Constructor>(constructor: T){
-        return class extends constructor {
-            version = params.version ? params.version : 'v1'
-            resource = params.resource ? params.resource : params
-        }
+  return function <T extends Constructor>(constructor: T){
+    return class extends constructor {
+      version = params.version ? params.version : 'v1'
+      resource = params.resource ? params.resource : params
+      gateway = params.gateway ? params.gateway : 'parking'
     }
+  }
 }
 
 export function route(resource: string) {
-    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        let value : string;
-        const getter = function() {
-            return value;
-        };
-        const setter = function(newVal: string) {
-            value = resource;
-        };
-        Object.defineProperty(target, 'resource', {
-            get: getter,
-            set: setter
-        })
-    }
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    let value : string;
+    const getter = function() {
+      return value;
+    };
+    const setter = function(newVal: string) {
+      value = resource;
+    };
+    Object.defineProperty(target, 'resource', {
+      get: getter,
+      set: setter
+    })
+  }
 }
 
 export function Min(limit: number) {
-    return function(target: unknown, propertyKey: string) {
-        let value : string;
-        const getter = function() {
-            return value;
-        };
-        const setter = function(newVal: string) {
-            if(newVal.length < limit) {
-                Object.defineProperty(target, 'errors', {
-                    value: `Your field should be bigger than ${limit}`
-                });
-            }
-            else {
-                value = newVal;
-            }
-        };
-        Object.defineProperty(target, propertyKey, {
-            get: getter,
-            set: setter
+  return function(target: unknown, propertyKey: string) {
+    let value : string;
+    const getter = function() {
+      return value;
+    };
+    const setter = function(newVal: string) {
+      if(newVal.length < limit) {
+        Object.defineProperty(target, 'errors', {
+          value: `Your field should be bigger than ${limit}`
         });
-    }
+      }
+      else {
+        value = newVal;
+      }
+    };
+    Object.defineProperty(target, propertyKey, {
+      get: getter,
+      set: setter
+    });
+  }
 }
 
 export function Max(limit: number) {
-    return function(target: unknown, propertyKey: string) {
-        let value : string;
-        const getter = function() {
-            return value;
-        };
-        const setter = function(newVal: string) {
-            if(newVal.length > limit) {
-                Object.defineProperty(target, 'errors', {
-                    value: `Your field should be smaller than ${limit}`
-                });
-            }
-            else {
-                value = newVal;
-            }
-        };
-        Object.defineProperty(target, propertyKey, {
-            get: getter,
-            set: setter
+  return function(target: unknown, propertyKey: string) {
+    let value : string;
+    const getter = function() {
+      return value;
+    };
+    const setter = function(newVal: string) {
+      if(newVal.length > limit) {
+        Object.defineProperty(target, 'errors', {
+          value: `Your field should be smaller than ${limit}`
         });
-    }
+      }
+      else {
+        value = newVal;
+      }
+    };
+    Object.defineProperty(target, propertyKey, {
+      get: getter,
+      set: setter
+    });
+  }
 }
 
