4562bc857b3b94fbb0162b3e88625993f5f1d162
BKlemm <bkl3mm@googlemail.com>
2022-01-11 20:06:52 +0100

    [AV-FE]: #implement shared-core:types handle configuration

--- frontend/libs/shared/core/src/lib/types/core.ts
+++ frontend/libs/shared/core/src/lib/types/core.ts
@@ -1,30 +1,31 @@
 import { Observable } from "rxjs";
+import {NavData, NavRoutes, TableFilter} from "./global";
 
 export interface ServiceInterface {
 
-    list(): Observable<unknown> ;
-    get(id: string): Observable<unknown>;
-    update(id: string, data: string): void;
-    create(data: string): void;
-    delete(id: string): void;
+  list(): Observable<unknown> ;
+  get(id: string): Observable<unknown>;
+  update(id: string, data: string): void;
+  create(data: string): void;
+  delete(id: string): void;
 }
 
 export class Observer {
-    next!: Function;
-    error!: Function;
-    complete!: Function;
+  next!: Function;
+  error!: Function;
+  complete!: Function;
 }
 
 export class SingleResponse {
-    public status!: number;
-    public IsSuccess!: boolean;
-    public details!: string;
-    public body!: unknown;
+  public status!: number;
+  public IsSuccess!: boolean;
+  public details!: string;
+  public body!: unknown;
 }
 
 export class Responses {
-    public errors: SingleResponse[] = [];
-    public success: SingleResponse[] = [];
+  public errors: SingleResponse[] = [];
+  public success: SingleResponse[] = [];
 }
 
 export interface Link {
@@ -42,7 +43,6 @@ export interface DataResponse<T> {
   pages?: Pageable
   links?: Link[]
 }
-
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface EntityInterface {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -61,4 +61,78 @@ export interface Column {
   link?: string,
   linkRef?: string,
   primaryState?: string
+}
+
+export class NavRoutesImpl {
+  action: string
+  title: string
+  generateLink: boolean
+  link?: string
+
+  constructor(action: string, title: string, generateLink: boolean = true) {
+    this.action = action
+    this.title = title
+    this.generateLink = generateLink
+  }
+}
+
+export class Navigation {
+  mainRoute: string
+  selector: string
+  navigation: Map<string, NavRoutesImpl> = new Map<string, NavRoutesImpl>()
+
+  constructor(route: string) {
+    this.mainRoute = route
+    this.selector = route
+  }
+
+  addSubroute(route: string) {
+    this.selector = route
+    return this
+  }
+
+  add(action: NavRoutesImpl) {
+    if (action.generateLink) {
+      action.link = this.mainRoute + '/' + this.selector
+    }
+    this.navigation.set(this.selector, action)
+    return this
+  }
+
+  build() {
+    return this.navigation.entries()
+  }
+}
+
+export abstract class Configuration {
+  navigation: Record<string, NavRoutes>
+  tabs: NavData[] = []
+  filter: TableFilter
+  displayedColumns: string[] = []
+  columns: Column[]
+
+  constructor() {
+    this.createNavigation()
+    this.createFilter()
+    this.createTableDefinition()
+
+    this.createTabs()
+    this.createDisplayedColumns()
+  }
+
+  private createTabs() {
+    for(const [key, value] of Object.entries(this.navigation)){
+      this.tabs.push(value['browse'])
+    }
+  }
+
+  private createDisplayedColumns() {
+    for(const value of this.columns) {
+      this.displayedColumns.push(value.columnDef)
+    }
+  }
+
+  abstract createFilter()
+  abstract createNavigation()
+  abstract createTableDefinition()
 }
