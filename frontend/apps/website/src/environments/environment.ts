07d55bfcf57c71ec151f66b3f6656a09abd73e27
BKlemm <bkl3mm@googlemail.com>
2022-01-11 19:59:52 +0100

    [AV-FE]: #change config:environment removed hardcoded gateway (will be resolved by route decorator logic)

--- frontend/apps/website/src/environments/environment.ts
+++ frontend/apps/website/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  endpoint: 'http://localhost:8098/api/',
+  endpoint: 'http://localhost:8098/',
   production: false
 };
 
