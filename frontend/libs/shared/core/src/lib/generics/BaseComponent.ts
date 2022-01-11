9d6b51dc6cecad773a100b71e68637cd19af9370
BKlemm <bkl3mm@googlemail.com>
2022-01-11 20:05:06 +0100

    [AV-FE]: #change shared-core:generics BaseComponent to handle lib configuration

--- frontend/libs/shared/core/src/lib/generics/BaseComponent.ts
+++ frontend/libs/shared/core/src/lib/generics/BaseComponent.ts
@@ -1,5 +1,6 @@
 import {Subscription} from "rxjs";
 import {Component, OnDestroy} from "@angular/core";
+import {Configuration} from "../types";
 
 @Component({template:''})
 export abstract class BaseComponent implements OnDestroy {
@@ -7,8 +8,10 @@ export abstract class BaseComponent implements OnDestroy {
   protected subscription: Subscription;
   protected routedId: string
 
+  protected constructor(private configuration?: Configuration) {}
+
   public trackBy(index: any, item: any) {
-      return index
+    return index
   }
 
   public ngOnDestroy(){
@@ -16,5 +19,25 @@ export abstract class BaseComponent implements OnDestroy {
       console.log('UNSUBSCRIBED')
       this.subscription.unsubscribe()
     }
+  }
+
+  get navigation() {
+    return this.configuration.navigation
+  }
+
+  get filter() {
+    return this.configuration.filter
+  }
+
+  get displayedColumns() {
+    return this.configuration.displayedColumns
+  }
+
+  get columns() {
+    return this.configuration.columns
+  }
+
+  get tabs() {
+    return this.configuration.tabs
   }
 }
