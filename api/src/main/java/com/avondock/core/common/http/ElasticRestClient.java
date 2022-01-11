c454e594921ab9a591ec1219ae518ea270e04f86
BKlemm <bkl3mm@googlemail.com>
2022-01-11 19:45:44 +0100

    [AV-API]: #change simplify elastic client

--- api/src/main/java/com/avondock/core/common/http/ElasticRestClient.java
+++ api/src/main/java/com/avondock/core/common/http/ElasticRestClient.java
@@ -1,7 +1,9 @@
 package com.avondock.core.common.http;
 
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class ElasticRestClient {
 
@@ -22,27 +25,36 @@ public class ElasticRestClient {
     }
 
     public Response get(String path, Object data) throws IOException {
-        Request req = new Request("GET", path);
-
-        if (data != null) {
-            req.setJsonEntity(new Gson().toJson(data));
-        }
-
-        return client.performRequest(req);
+        return this.request(path, data, "GET");
     }
 
-    public void post(String path, Object data) throws IOException {
-        Request req = new Request("POST", path);
+    public void post(String path, Object data) {
+        this.request(path, data, "POST");
+    }
 
-        if (data != null) {
-            req.setJsonEntity(new Gson().toJson(data));
-        }
-
-        client.performRequest(req);
+    public void put(String path, Object data) {
+        this.request(path, data, "PUT");
     }
 
     public void delete(String path) throws IOException {
         Request req = new Request("DELETE", path);
         client.performRequest(req);
+    }
+
+    public Response request(String path, Object data, String method) {
+        Request req = new Request(method, path);
+
+        if (data != null) {
+            req.setJsonEntity(new Gson().toJson(data));
+        }
+
+        try {
+            Response res = client.performRequest(req);
+            System.out.println(EntityUtils.toString(res.getEntity()));
+            return res;
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+        return null;
     }
 }
