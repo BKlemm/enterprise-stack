f4922c7e1f32e6e79734e5f3960eb322f220e602
BKlemm <bkl3mm@googlemail.com>
2022-01-12 19:14:54 +0100

    [AV-FE]: #extend shared-core:data country data mock

---
+++ frontend/libs/shared/core/src/lib/data/country.data.ts
@@ -0,0 +1,21 @@
+export const COUNTRIES = [
+  {label:"Deutschland", regions: [
+      {label:"Hessen"},
+      {label:"Bayern"},
+      {label:"Bremen"},
+      {label:"Berlin"}
+    ]
+  },
+  {label:"Polen", regions:[
+      {label:"Schlesien"},
+      {label:"Danzig"}
+    ]
+  }
+]
+
+export const REGIONS = [
+  {label:"Hessen"},
+  {label:"Bayern"},
+  {label:"Bremen"},
+  {label:"Berlin"}
+]
--- frontend/libs/shared/core/src/lib/data/index.ts
+++ frontend/libs/shared/core/src/lib/data/index.ts
@@ -1,1 +1,4 @@
+import * as COUNTRY_DATA from './country.data'
+export {COUNTRY_DATA}
+
 export {ObjectMapper} from "./objectmapper";
