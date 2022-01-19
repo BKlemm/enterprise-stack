1a48eabba32afeea639ecd44816bfbc690103efe
BKlemm <bkl3mm@googlemail.com>
2022-01-19 19:12:38 +0100

    [AVAPI]: #extend class:HttpFilter because of java optional empty string is optional empty null

--- api/src/main/java/com/avondock/core/common/http/HttpFilter.java
+++ api/src/main/java/com/avondock/core/common/http/HttpFilter.java
@@ -3,12 +3,25 @@ package com.avondock.core.common.http;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class HttpFilter {
-    String filter = "all";
+
+    public static String FILTER_ALL = "all";
+
+    String filter = FILTER_ALL;
     String sortDirection = "asc";
     String activeSort = "";
     Integer pageIndex = 0;
     Integer pageSize = 20;
+
+    public boolean hasActiveSort() {
+        return !Objects.equals(this.activeSort, "") && this.activeSort != null;
+    }
+
+    public boolean isFilterable() {
+        return filter != null && !filter.equals(FILTER_ALL) && !filter.equals("");
+    }
 }
